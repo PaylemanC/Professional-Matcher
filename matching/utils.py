ac9087ac9e33970581480bc0ae8ab9df48801dba
Carina <carinapaylemandev@gmail.com>
2025-07-12 16:06:22 -0300

    Add stop words clean to clean_job_offer

--- matching/utils.py
+++ matching/utils.py
@@ -1,5 +1,8 @@
 import re # Pattern: [^a-z0-9\s] -> Letters, numbers and spaces
 import unicodedata
+import spacy
+
+nlp = spacy.load("es_core_news_sm")  
 
 def clean_job_offer(job_offer):
     job_offer = unicodedata.normalize('NFKD', job_offer).encode('ascii', 'ignore').decode('utf-8')
@@ -8,7 +11,10 @@ def clean_job_offer(job_offer):
 
     job_offer = re.sub(r'[^a-z0-9\s]', '', job_offer)
 
+    job_offer_doc = nlp(job_offer)
+    job_offer = ' '.join([word.text for word in job_offer_doc 
+                          if not word.is_stop])  
+
     return job_offer
 
-example = clean_job_offer("Software Engineer - Python & Django - Oferta de Trabajo!")  
-print(example)
+print(clean_job_offer("Software Engineer - Python & Django - Oferta de Trabajo increíble...!"))
