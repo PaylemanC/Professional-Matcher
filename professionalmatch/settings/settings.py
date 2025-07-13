d1c868aae49ea510c678c6f495904e789ad444ff
Carina <carinapaylemandev@gmail.com>
2025-07-13 00:25:32 -0300

    Restructure settings.py: local.py is now necessary to run the manage.py

--- professionalmatch/settings/settings.py
+++ professionalmatch/settings/settings.py
@@ -9,6 +9,7 @@ https://docs.djangoproject.com/en/5.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
+from .local import *
 
 from pathlib import Path
 import os
@@ -23,13 +24,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-4ak2q-_)8fa4s32(73@l&4^78f^kba7g2g=t!5c-up@gpt=_o9'
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
 ALLOWED_HOSTS = []
 
 
 # Application definition
+
 
 DJANGO_APPS = [
     'django.contrib.admin',
@@ -67,7 +66,7 @@ ROOT_URLCONF = 'professionalmatch.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')],
+        'DIRS': [os.path.join(BASE_DIR.parent, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -85,12 +84,7 @@ WSGI_APPLICATION = 'professionalmatch.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+# DATABASE from LOCAL.PY
 
 # Authentication settings
 
