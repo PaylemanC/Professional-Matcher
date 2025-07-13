7afcb3f6d0013a0e8a32a4974d3c7f7478c784d1
Carina <carinapaylemandev@gmail.com>
2025-07-13 14:37:13 -0300

    Create basic technologies fixture file

--- technologies/models.py
+++ technologies/models.py
@@ -3,6 +3,10 @@ from django.db import models
 class Technology(models.Model):
     name = models.CharField(max_length=100, unique=True)
     
+    def save(self, *args, **kwargs):
+        self.name = self.name.lower()
+        super().save(*args, **kwargs)
+    
     def __str__(self):
         return self.name
     
