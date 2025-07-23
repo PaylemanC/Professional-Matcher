928664d42a9a727637514791e5e150929124bca3
Carina <carinapaylemandev@gmail.com>
2025-07-22 22:45:01 -0300

    Restore password validators

--- professionalmatch/settings/settings.py
+++ professionalmatch/settings/settings.py
@@ -95,9 +95,12 @@ LOGIN_URL = '/users/login'  # @login_required
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
-    # {
-    #     'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
-    # },
+    {
+        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+    },
     {
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
